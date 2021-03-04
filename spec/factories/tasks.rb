2077f57cba312657b1bbda04be18201da682c5d3
UMA-no9 <be.peru.ponkot@gmail.com>
2021-03-04 16:40:08 +0900

    Modified: spec/models/task.rb for validations

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  factory :task do
-    
+  factory :task do    
+    sequence(:title) { |n| "task_title(#{n})" }
+    content { "task_content" }
+    status { 0 }
+    deadline { nil }
   end
 end
